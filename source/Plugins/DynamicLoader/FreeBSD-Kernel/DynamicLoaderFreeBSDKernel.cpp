aae6fd38026d0ea6c0d96f582a8eedeb2071a294
Mike Ma <mikemandarine@gmail.com>
2014-08-14 23:56:20 +0200

    use alternative Module constructor

--- source/Plugins/DynamicLoader/FreeBSD-Kernel/DynamicLoaderFreeBSDKernel.cpp
+++ source/Plugins/DynamicLoader/FreeBSD-Kernel/DynamicLoaderFreeBSDKernel.cpp
@@ -183,8 +183,9 @@ DynamicLoaderFreeBSDKernel::LoadAllCurrentModules()
         if (FindKLDPath(kld_filename, path)) {
             addr_t kld_addr = ReadPointer (kaddr + m_address_offset);
             FileSpec file_spec (path.c_str(), true);
-            ConstString objfile (path.c_str());
-            ModuleSP module_sp(new Module(file_spec, arch_spec, &objfile, kld_addr));
+            // ConstString objfile (path.c_str());
+            ModuleSpec module_spec(file_spec, arch_spec);
+            ModuleSP module_sp(new Module(module_spec));
             module_list.AppendIfNeeded(module_sp);
         }
     }
